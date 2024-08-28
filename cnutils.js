bb1110d2b785ba6eaa11d29dfc68abe8c24fcee0
cnaegele <cnaegele@lausanne.ch>
2024-08-28 11:51:55 +0200

    detecte objet identique dans array d'objet

--- cnutils.js
+++ cnutils.js
@@ -3,3 +3,17 @@ export function bDateFutur(date) {
     const date2test = new Date(date)
     return date2test > datenow
 }
+
+export function detectIdenticalObjects(arr) {
+    const stringified = arr.map(obj => JSON.stringify(obj));
+    const uniqueStringified = new Set(stringified);
+    const duplicates = stringified.filter(item => {
+      if (uniqueStringified.has(item)) {
+        uniqueStringified.delete(item);
+        return false;
+      }
+      return true;
+    });
+    
+    return duplicates.map(JSON.parse);
+}
