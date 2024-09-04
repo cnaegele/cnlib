d29e7f353159d4c23e74fb8a8dc5b2a6ea88bb85
cnaegele <cnaegele@lausanne.ch>
2024-09-04 12:04:54 +0200

    test calidite de date

--- cnutils.js
+++ cnutils.js
@@ -1,3 +1,10 @@
+export function bDateValid(date) {
+    const date2test = new Date(date)
+    // An invalid date object returns NaN for getTime() and NaN is the only
+    // object not strictly equal to itself.
+    return date2test.getTime() === date2test.getTime()
+}
+
 export function bDateFutur(date) {
     const datenow = new Date()
     const date2test = new Date(date)
